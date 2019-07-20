5560a6b7587c061b5726311e6aaf0c4e4a80a25a
Zak <elseht17@gmail.com>
2019-07-19 23:20:42 -0400

    added two webpages, a get request, and a sample webpage just for practice

--- Stock_Box/Stock_App/urls.py
+++ Stock_Box/Stock_App/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('settings', views.settings, name = 'settings')
+    path('settings', views.settings, name = 'settings'),
+    path('ayo', views.ayo_home, name = "ayo_home"),
+    path('add', views.add, name = 'add')
 ]
--- Stock_Box/Stock_App/views.py
+++ Stock_Box/Stock_App/views.py
@@ -3,17 +3,23 @@ from django.http import HttpResponse
 import csv
 # Create your views here.
 
-
+# PAGES
 def home(request):
     #response = HttpResponse("Hello world, this is the home page")
-    response = render(request, 'home.html')
-    return response
+    # pass the request, html file, and dicitonary holding any info, 
+    # that can be accesed by calling the dictionaries index like,
+    # for example, {{name}} (in html file) for a dict {'name':'Zak'}
+    # passed as a third parameter  
+    return render(request, 'home.html', {'name':'Zak'})
 
 def settings(request):
     return HttpResponse("Settings WIP")
 
 def sign_in(request):
     return HttpResponse("WIP")
+
+def ayo_home(request):
+    return render(request, 'web_practice_1.html')
 
 def visual_examples(request):
     # Example graphs using MSFT df and a HTML/CSV header
@@ -24,3 +30,11 @@ def visual_examples(request):
     writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
     writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
     return response
+
+# ACTIONS
+def add(request):
+
+    val_1 = request.GET['num_1']
+    val_2 = request.GET['num_2']
+    res = int(val_1) + int(val_2)
+    return render(request, 'result.html', {'result':res})
