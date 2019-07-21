b04968df990e25e247cf38e5ce82e308fc6de909
Zak <elseht17@gmail.com>
2019-07-20 23:25:15 -0400

    removed some useless css files and edited the ayo application html along with its models.py file

--- Stock_Box/ayo_test/views.py
+++ Stock_Box/ayo_test/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hi")
+    
+    dest_1 = Destination()
+    dest_1.name = 'Issac'
+    dynamic_dict = {
+        'long_text' : """NOTE**: THIS IS JUST SAMPLE TEXT TO SHOW THAT THIS IS A DYNAMIC 
+                    TEXT SECTION FOR WHATEVER USES YOU MAY HAVE ~ Zak \n\n
+                    "Whatever may come, fast or progressive, forcefully or quietly, abrasive or other. 
+                    Do not waiver, do not weaken, do not fall. Failure may feel like
+                    the heaviest weight, but in reality, nothing is heavier than despair" """
+    }             
+    dynamic_dict[dest_1] = dest_1
+ 
+    #return render(request, 'web_practice_1.html', dynamic_dict)
+    return render(request, 'web_practice_1.html', {'dest_1':dest_1})
+
+    # Custon function
